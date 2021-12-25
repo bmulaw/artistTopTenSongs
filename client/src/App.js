5b7803b187b5e895e2530d864e626f38e1b2b505
unknown <bmulaw@bu.edu>
2021-12-24 18:08:46 -0800

    app.js is class component now

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,32 @@
-import React, { useState, useEffect } from 'react';
+import React, { Component } from 'react';
 import Form from './components/Form'
 import './App.css';
 
-function App() {
-  const [status, getStatus] = useState("not working");
-  // source https://blog.miguelgrinberg.com/post/how-to-create-a-react--flask-project/page/4
-  useEffect(() => {
-    fetch('/test')
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      "status": "not working"
+    }
+  }
+
+  getStatus = () => {
+    fetch("/test")
     .then(res => res.json())
     .then(data => {
-      console.log(data);
-      getStatus(data.status);
-    });
-  }, []);
+      this.setState({status: data["status"]})
+    })
+    console.log(this.state.status)
+  }
 
-  return (
+  render() {
+    this.getStatus();
+    return (
     <div className="App">
       <header className="App-header">
-        <p> The connection between React and Flask is {status}</p>
+        <p> The connection between React and Flask is {this.state.status}</p>
         <Form/>
       </header>
     </div>
-  );
+    )};
 }
-
-export default App;
